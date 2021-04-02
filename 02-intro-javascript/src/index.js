95a899638e4305756b6c17b8e4046e96d2bc12cd
Santiago Yanguas <santitoya-2001@outlook.com>
2021-04-02 17:40:02 -0500

    [Feature] Intro js

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,10 @@
+const nombre = "Santiago";
+const apellido = "Yanguas";
+
+const nombreCompleto = `${nombre} ${apellido} ${1 + 1}`;
+
+console.log(nombreCompleto);
+
+function getSaludo() {
+    
+}
