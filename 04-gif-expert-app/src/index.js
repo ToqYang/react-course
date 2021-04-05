28154925af19b9addf90eb9a05a301b144736022
Santiago Yanguas <santitoya-2001@outlook.com>
2021-04-05 13:12:43 -0500

    [Feature] Save cha

--- 04-gif-expert-app/src/index.js
+++ 04-gif-expert-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import GifExpertApp from "./GifExpertApp";
 import "./index.css";
 
@@ -9,4 +9,4 @@ ReactDOM.render(<GifExpertApp />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
