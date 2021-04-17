21f8ffd9e1c904a5228848d6b46544b74a5a6e27
Santiago Yanguas <santitoya-2001@outlook.com>
2021-04-17 17:59:06 -0500

    [Feature] Make and show heroes app

---
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -0,0 +1,17 @@
+import React, { useMemo } from "react";
+import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+import HeroCard from "./HeroCard";
+
+const HeroList = ({ publisher }) => {
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
+  //const heroes = getHeroesByPublisher(publisher);
+  return (
+    <div className="card-columns animate__animated animate__fadeIn">
+      {heroes.map((heroe) => {
+        return <HeroCard key={heroe.id} {...heroe} />;
+      })}
+    </div>
+  );
+};
+
+export default HeroList;
